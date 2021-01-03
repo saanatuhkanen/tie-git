9a00ac59dac829298356003ee447ca1d16164f27
Saana <74203712+saanatuhkanen@users.noreply.github.com>
2021-01-03 15:41:27 +0200

    Add HelloWorld form feature/create-awesome

--- hello_world.py
+++ hello_world.py
@@ -3,3 +3,7 @@ print("Hello World!")
 =======
 print("Hello world!")
 >>>>>>> branch_repo/master
+print("Hello from feature")
+>>>>>>> feature/create-awesome
+print("Hello from release")
+>>>>>>> release
