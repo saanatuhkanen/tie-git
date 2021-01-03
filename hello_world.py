95d121915c1eb85a48d578fb8624810679f8eea9
saanatuhkanen <74203712+saanatuhkanen@users.noreply.github.com>
2021-01-03 15:51:42 +0200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 print("Hello World!")
-=======
 print("Hello world!")
->>>>>>> branch_repo/master
 print("Hello from feature")
->>>>>>> feature/create-awesome
-print("Hello from release")
->>>>>>> release
